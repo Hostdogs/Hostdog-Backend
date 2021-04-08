49171b3062b53a203a86fa587c22a192d8d33063
pjpure <pure254326@gmail.com>
2021-04-09 00:31:06 +0700

    api dog

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from accounts.models import Accounts
+from accounts.models import Dog
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -9,9 +10,17 @@ class AccountSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Accounts
-        fields = ("username", "email", "password", "first_name", "last_name", "mobile", "address", "role")
+        fields = ("id","username", "email", "password", "first_name", "last_name", "mobile", "address", "role")
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         account = Accounts.objects.create_user(**validated_data)
         return account
+
+class DogSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Dog
+        fields = ("id","customer_id","dog_name","dog_dob","dog_breed","dog_weight","dog_bio")
+        extra_kwargs = {"dog_name":required=True}
+        
--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,13 @@ from accounts.models import Accounts
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from accounts.models import Dog
 
 
 class AccountsViewSet(viewsets.ModelViewSet):
     queryset = Accounts.objects.all()
     serializer_class = AccountSerializer
+
+class DogViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
