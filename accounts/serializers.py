e3ff69148893990e8f4509f74b20e4b2a2bb37ad
chuckboliver <patpumhak@gmail.com>
2021-04-09 00:35:44 +0700

    add host and customer serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from accounts.models import Accounts
+from accounts.models import Accounts, Customer, Host
 
 
 class AccountSerializer(serializers.ModelSerializer):
@@ -9,9 +9,55 @@ class AccountSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Accounts
-        fields = ("username", "email", "password", "first_name", "last_name", "mobile", "address", "role")
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "mobile",
+            "address",
+            "role",
+        )
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         account = Accounts.objects.create_user(**validated_data)
         return account
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for customer
+    """
+
+    class Meta:
+        model = Customer
+        fields = (
+            "id",
+            "account_id",
+            "customer_bio",
+            "customer_dog_count",
+            "customer_hosted_count",
+        )
+
+
+class HostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for host
+    """
+
+    class Meta:
+        model = Host
+        fields = (
+            "id",
+            "account_id",
+            "host_bio",
+            "host_rating",
+            "host_hosted_count",
+            "host_max",
+            "host_avaliable",
+            "host_area",
+            "host_schedule",
+        )
