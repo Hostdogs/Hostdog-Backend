c19d75dcf35a6d9c3a8111fb2870a22819184af9
chuckboliver <patpumhak@gmail.com>
2021-04-08 22:03:54 +0700

    delete comment

--- accounts/models.py
+++ accounts/models.py
@@ -9,38 +9,6 @@ from django.contrib.auth.models import (
 )
 # Create your models here.
 
-# class CustomAccountManager(BaseUserManager):
-#     """
-#     Account manager
-#     """
-#     def create_superuser(self, email, username, password, firstname, lastname, **other_fields):
-#         """
-#         Create super user
-#         """
-#         other_fields.setdefault("is_staff", True)
-#         other_fields.setdefault("is_superuser", True)
-#         other_fields.setdefault("is_active", True)
-
-#         if other_fields.get("is_staff") is not True:
-#             raise ValueError("Superuser must be assigned to is_staff=True.")
-#         if other_fields.get("is_superuser") is not True:
-#             raise ValueError("Superuser must be assigned to is_superuser=True.")
-#         return self.create_user(email, username, password, firstname, lastname, **other_fields)
-
-
-#     def create_user(self,email, username, password, firstname, lastname, **other_fields):
-#         """
-#         Create user
-#         """
-#         if not email:
-#             raise ValueError(gettext_lazy("You must provide an email address"))
-#         email = self.normalize_email(email)
-#         user = self.model(
-#             email=email, username=username, firstname=firstname, lastname=lastname, **other_fields
-#         )
-#         user.set_password(password)
-#         user.save()
-#         return user
 
 
 class Accounts(AbstractUser):
