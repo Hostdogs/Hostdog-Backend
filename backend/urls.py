6ef344532b92045306922669c2233d462112d9d8
chuckboliver <patpumhak@gmail.com>
2021-04-10 23:10:07 +0700

    - delete TemplateView - use doc url as home page

--- backend/urls.py
+++ backend/urls.py
@@ -19,14 +19,12 @@ from accounts.models import Accounts
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),
     path("api/", include("accounts.urls", namespace="accounts")),
     path(
         "schema/",
@@ -35,5 +33,5 @@ urlpatterns = [
         ),
         name="openapi-schema",
     ),
-    path("docs/", include_docs_urls(title="Hostdog-API")),
+    path("", include_docs_urls(title="Hostdog-API")),
 ]
