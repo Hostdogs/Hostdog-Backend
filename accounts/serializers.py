b9173ed6f8aaefa2789d6881c0339f9a8d8440a2
pjpure <pure254326@gmail.com>
2021-04-10 17:22:36 +0700

    account

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,42 @@
 from rest_framework import serializers
 from accounts.models import Accounts, Customer, Host, Dog
-
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class AccountSerializer(serializers.ModelSerializer):
     """
     Serializer for account
     """
+    class Meta:
+        model = Accounts
+        fields = (
+            "id",
+            "is_customer",
+            "is_host",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "dob",
+            "mobile",
+            "address",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            }
 
+    def create(self, validated_data):
+        account = Accounts.objects.create_user(**validated_data)
+        return account
+
+
+
+class CustomerAccountSerializer(serializers.ModelSerializer):
+    """
+    Serializer for account
+    """
     class Meta:
         model = Accounts
         fields = (
@@ -17,30 +46,52 @@ class AccountSerializer(serializers.ModelSerializer):
             "password",
             "first_name",
             "last_name",
+            "dob",
             "mobile",
             "address",
-            "role",
         )
-        extra_kwargs = {"password": {"write_only": True}}
+        extra_kwargs = {
+            "password": {"write_only": True},
+            }
 
     def create(self, validated_data):
+        validated_data['is_customer'] = True
         account = Accounts.objects.create_user(**validated_data)
+        Customer.objects.create(account=account)
         return account
 
-
-class DogSerializer(serializers.ModelSerializer):
-
+class HostAccountSerializer(serializers.ModelSerializer):
+    """
+    Serializer for account
+    """
     class Meta:
-        model = Dog
-        fields = ("id","customer_id","dog_name","dog_dob","dog_breed","dog_weight","dog_bio")
-        
+        model = Accounts
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "dob",
+            "mobile",
+            "address",
+        )
         extra_kwargs = {
-            "dog_name":{"required":True},
+            "password": {"write_only": True},
         
-        }
+            }
+
+    def create(self, validated_data):
+        validated_data['is_host'] = True
+        account = Accounts.objects.create_user(**validated_data)
+        Host.objects.create(account=account)
+        return account
         
 
-class CustomerSerializer(serializers.ModelSerializer):
+
+
+class CustomerProfileSerializer(serializers.ModelSerializer):
     """
     Serializer for customer
     """
@@ -48,15 +99,19 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = (
-            "id",
-            "account_id",
+            "account",
             "customer_bio",
             "customer_dog_count",
             "customer_hosted_count",
         )
 
+    def create(self, validated_data):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-class HostSerializer(serializers.ModelSerializer):
+    
+
+
+class HostProfileSerializer(serializers.ModelSerializer):
     """
     Serializer for host
     """
@@ -64,8 +119,7 @@ class HostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Host
         fields = (
-            "id",
-            "account_id",
+            "account",
             "host_bio",
             "host_rating",
             "host_hosted_count",
@@ -75,3 +129,17 @@ class HostSerializer(serializers.ModelSerializer):
             "host_schedule",
         )
 
+    def create(self, validated_data):
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class DogProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Dog
+        fields = ("id","customer_id","dog_name","dog_dob","dog_breed","dog_weight","dog_bio")
+        
+        extra_kwargs = {
+            "dog_name":{"required":True},
+        
+        }
--- accounts/views.py
+++ accounts/views.py
@@ -1,48 +1,58 @@
-from accounts.serializers import AccountSerializer, CustomerSerializer, HostSerializer,DogSerializer
-from accounts.models import Accounts, Customer, Host
+from accounts.serializers import AccountSerializer,CustomerAccountSerializer, HostAccountSerializer, HostProfileSerializer,CustomerProfileSerializer,DogProfileSerializer
+from accounts.models import Accounts, Customer, Host,Dog
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from accounts.models import Dog
 
 
-class AccountsViewSet(viewsets.ModelViewSet):
+
+class AccountViewSet(viewsets.ModelViewSet):
     """
     API endpoint for query account
     """
     queryset = Accounts.objects.all()
-    serializer_class = AccountSerializer
+    serializer_class = AccountSerializer 
+
+class CustomerAccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for query account
+    """
+    queryset = Accounts.objects.filter(is_customer=True)
+    serializer_class = CustomerAccountSerializer
+
+class HostAccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for query account
+    """
+    queryset = Accounts.objects.filter(is_host=True)
+    serializer_class = HostAccountSerializer
 
 
-class DogViewSet(viewsets.ModelViewSet):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
 
-
-class CustomerViewSet(viewsets.ModelViewSet):
+class CustomerProfileViewSet(viewsets.ModelViewSet):
     """
     API endpoint for query customer
     """
     queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+    serializer_class = CustomerProfileSerializer
     
-    def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get("pk")
-        return generics.get_object_or_404(Customer, id=item)
-
-    
+    # def get_object(self, queryset=None, **kwargs):
+    #     item = self.kwargs.get("pk")
+    #     return generics.get_object_or_404(Customer, id=item)
 
 
-class HostViewSet(viewsets.ModelViewSet):
+class HostProfileViewSet(viewsets.ModelViewSet):
     """
     API endpoint for query host
     """
     queryset = Host.objects.all()
-    serializer_class = HostSerializer
+    serializer_class = HostProfileSerializer
 
-    def get_object(self, queryset=None, **kwargs):
-        item = self.kwargs.get("pk")
-        return generics.get_object_or_404(Host, id=item)
+    # def get_object(self, queryset=None, **kwargs):
+    #     item = self.kwargs.get("pk")
+    #     return generics.get_object_or_404(Host, id=item)
 
-    
 
+class DogProfileViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogProfileSerializer
