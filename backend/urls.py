ab42a2dfb6eb445927da41548143ea2200e28449
chuckboliver <patpumhak@gmail.com>
2021-04-10 16:50:20 +0700

    add coreapi doc

--- backend/urls.py
+++ backend/urls.py
@@ -17,13 +17,23 @@ Including another URLconf
 
 from accounts.models import Accounts
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='index.html')),
-    path('api/', include('accounts.urls', namespace='accounts'))
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("api/", include("accounts.urls", namespace="accounts")),
+    path(
+        "schema/",
+        get_schema_view(
+            title="Hostdog-API", description="API for Hostdog system", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
+    path("docs/", include_docs_urls(title="Hostdog-API")),
 ]
