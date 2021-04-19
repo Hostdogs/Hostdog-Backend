42070fe80820cf10fd8fe7c8952108923bcce69c
chuckboliver <patpumhak@gmail.com>
2021-04-19 18:39:31 +0700

    add some doc string

--- service/views.py
+++ service/views.py
@@ -10,11 +10,18 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing service
+        - Customer request service to Host (POST) --> notification to host
+    """
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 
 class MealViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing meal of service
+    """
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
     filter_backends = [DjangoFilterBackend]
@@ -22,11 +29,17 @@ class MealViewSet(viewsets.ModelViewSet):
 
 
 class HostServiceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing host service
+    """
     queryset = HostService.objects.all()
     serializer_class = HostServiceSerializer
 
 
 class ChatViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for managing chat
+    """
     queryset = Chat.objects.all()  # .order_by('chat_date_time')
     serializer_class = ChatSerializer
     filter_backends = [DjangoFilterBackend]
