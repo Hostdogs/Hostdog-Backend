d06d4870ae0057ac0f275ffe84959f4a49537548
chuckboliver <patpumhak@gmail.com>
2021-04-23 16:18:33 +0700

    add thing to do

--- service/tests.py
+++ service/tests.py
@@ -4,15 +4,54 @@ from django.contrib.auth.models import (
 )
 from accounts.models import Host,Customer,Dog,Accounts
 from service.models import Service,Meal
+from rest_framework.test import APIClient, APITestCase
 
 #py manage.py makemigrations
 #py manage.py migrate
 #coverage run --omit="*/hostdog/*" manage.py test
 #coverage html
-class TestModel(TestCase):
-
+class API_Testing(APITestCase):
+    """
+    TODO:
+        - Model testing
+            * Test about create the object model (SUCCESS)
+        - Host create available date (>= 3 Testcase)
+            * register date in the past (FAIL)
+            * register date greater than or equal to today (SUCCESS)
+            * register date that in service (FAIL)
+        - ServiceViewSet
+            - Customer register the host service 
+                * register to the correct host available date (SUCCESS)
+                * register to the wrong host available date (FAIL)
+                * register to the date in the past (FAIL)
+                * attribute in service object must correct (SUCCESS)
+                * customer choose wrong additional service (FAIL)
+                    @ Host เลือกไม่เปิด Field ตามนี้ แต่ customer กลับเลือกใช้บริการ
+                    @ is_dog_walk
+                    @ is_get_dog
+                    @ is_delivery_dog
+                    @ is_bath_dog
+                * customer choose correct additional service (SUCCESS)
+                    @ ตรงข้ามกับข้อก่อนหน้า
+                * customer select the correct meal (SUCCESS)
+                * customer select the wrong meal (FAIL)
+                * after finish registration main_status is pending (SUCCESS)
+            - Host response to the customer request
+                * Host accept the customer request then main_status change to payment (SUCCESS)
+                * Host decline the customer request then main_status change to cancelled (SUCCESS)
+                * Host accept request ครั้งที่ 2 (FAIL) **ควรจะได้ครั้งเดียว
+                * Host decline request ครั้งที่ 2 (FAIL) **ควรจะได้ครั้งเดียว
+        - MealViewSet
+            * สร้างมื้ออาหารได้ (SUCCESS)
+            * ปรับราคาได้ (SUCCESS)
+        - HostServiceViewSet
+            * host ปรับ enable/disable additional service ได้
+            * host ปรับราคา additional service ได้
+            * host เพิ่ม/ลบ อาหารจาก Meal ได้
+    """
     @classmethod
     def setUpTestData(self):
+        #Model testing
         acc_005 = Accounts.objects.create(
             is_superuser=False,
             password='123123123',
@@ -24,7 +63,6 @@ class TestModel(TestCase):
             date_joined='2021-04-13 14:37:50.965870+00:00',
             is_host=False
         )
-
         host_002 = Host.objects.create(
             first_name='first',
             last_name='last',
@@ -49,7 +87,22 @@ class TestModel(TestCase):
             host_id=host_002.account_id
             
         )
-        
+
+        #create account for API testing
+        acc_api_001 = Accounts.objects.create_user(
+            username="Account_001",
+            email="acc_001@api.com",
+            password="123",
+            is_host=False
+        )
+        acc_api_002 = Accounts.objects.create_user(
+            username="Account_002",
+            email="acc_002@api.com",
+            password="123",
+            is_host=True
+        )
+
+
     def test_meal(self):
         meal = Meal.objects.get(host_id=5)
 
